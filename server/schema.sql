a2711384eb1fac66647e2e6e5920a64c406151de
Kaijie Zheng <kaijiejz50@gmail.com>
2015-10-30 14:33:18 -0700

    Add notes on how to drop all tables

--- server/schema.sql
+++ server/schema.sql
@@ -7,6 +7,18 @@ CREATE TABLE messages (
 );
 
 /* Create other tables and define schemas for them here! */
+-- method 1
+-- SELECT 'DROP TABLE [' + SCHEMA_NAME(schema_id) + '].[' + name + ']' FROM sys.tables
+
+-- method 2
+-- SET @tables = NULL;
+-- SELECT GROUP_CONCAT('`', table_schema, '`.`', table_name,'`') INTO @tables FROM information_schema.tables 
+--   WHERE table_schema = 'myDatabase' AND table_name LIKE BINARY 'del%';
+
+-- SET @tables = CONCAT('DROP TABLE ', @tables);
+-- PREPARE stmt1 FROM @tables;
+-- EXECUTE stmt1;
+-- DEALLOCATE PREPARE stmt1;
 
 
 
